337236d899be3a831abbcabc19d0194e1621f889
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2017-02-12 11:37:16 -0200

    Migration for Person

--- sysfolk/core/models.py
+++ sysfolk/core/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
 class Person(models.Model):
-    name = models.CharField('nome', max_length=255)
+    name = models.CharField(_('name'), max_length=255)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _('person')
+        verbose_name_plural = _('persons')
